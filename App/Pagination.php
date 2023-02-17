949f61ef8929823eae8b6a02439af519763ce938
neecride <neecride@gmail.com>
2023-02-17 19:47:14 +0100

    Update Pagination.php

--- App/Pagination.php
+++ App/Pagination.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use PDO;
 
 Class Pagination extends Database{
 	
@@ -10,15 +11,6 @@ Class Pagination extends Database{
 	*/
 	private function Params(){
 		return new Parameters();
-	}	
-
-	public function RequestTarget(){
-
-		if(isset($_GET['page'])){
-			$getpage = (int) $_GET['page'];
-			return $getpage;
-		}
-
 	}
 	
 	//si besoin on a notre objet pdo
@@ -26,158 +18,135 @@ Class Pagination extends Database{
 		return $this->Getpdo();
 	}
 
-	/*
-	* return instance PDO
-	*/
-	private function Cnx(){
-		return new Database();
-	}		
+	public function Perpage(){
+		return (int) $this->Params()->GetParam(2);
+	}
 
-	/*
-	* return pagemax pagination
-	*/
-	private function PagesMax(){
+	public function subLinkPage($url,$countid){ 
+		
+		$tt = ceil($countid/$this->Params()->GetParam(2));
 
-		//$this->Params()->GetParam(2)
+		if($tt > 1){
+		  for($ii=1; $ii <= $tt; $ii++){ 
 
-		return $this->Params()->GetParam(2);
+				if($ii == 1){
+
+					echo "<a href=".$url.">$ii</a>";
+					
+				}else{
+
+					echo "<a href=".$url.'?page='. $ii.">$ii</a>";
+				}
+ 
+			}
+		}
 
 	}
 
-	public function CountPage($statement,$attr=null){
+	public function userLinkPage($url,$id,$countid){
+				
+		$tt = ceil($countid/$this->Params()->GetParam(2));
 
-
-			$req = $this->Getpdo()->prepare($statement);
-			
-			$req->execute($attr);
-
-			$results = $req->rowCount();
-
-			return $results;
+		if($this->Params()->GetParam(2) >= $tt){
+			if($tt == 1){
+			  $linkRedirectPage =  $url.'#rep-' . $id;
+			}else{
+			  $linkRedirectPage =   $url.'?page='.$tt.'#rep-' . $id;
+			}
+		}
+		return $linkRedirectPage;
 
 	}
 
+
+	public function offset($PerPage,$CurrentPage){
+
+		return $PerPage * ($CurrentPage - 1);
+
+	}
+	
+	public function isExistPage($CurrentPage, $pages,$url){
+		if($CurrentPage > $pages && $CurrentPage > 1) {
+			setflash("Ce numéro de page n'hexiste pas","orange");
+			header('Location:' . $url);
+			http_response_code(301);
+			exit();
+		}
+	}
 
 	/*
 	* return count article in database
 	*/
-	public function CountData(){
+	public function CountIDForpagination($statement,$attr=null)
+	{
+		if($attr != null){
 
-		return $this->Cnx()->CountID('SELECT id FROM f_topics');
+			$smtp = $this->Getpdo()->prepare($statement);
 
-	}
+			$smtp->execute([intval($attr)]);
 
-	/*
-	* return split count articles and pagemax pagination
-	*/
-	private function PageTotales(){
+			$Count = (int) $smtp->fetch(PDO::FETCH_NUM)[0];
 
-		return ceil($this->CountData()/$this->PagesMax());
-	}
+			return $Count;
 
-	/*
-	* return star loop pagination
-	*/
-	private function StartPager(){
+		}else{
+			$Count = (int) $this->Getpdo()->query($statement)->fetch(PDO::FETCH_NUM)[0];
 
-		return ($this->CurrentPage()-1)*$this->PagesMax();
-
-	}
-
-
-	/*
-	* return pagination limit
-	*/
-	public function Limited(){
-
-		return $this->StartPager() .','. $this->PagesMax();
-
-	}
-
-	/*
-	* return current page where get ppagination 
-	*/
-	private function CurrentPage(){
-
-		if($this->RequestTarget() > 0 AND $this->RequestTarget() <= $this->PageTotales()) {
-		    
-		   $CurrentPage = $this->RequestTarget();
-		    
-		} else {
-		    //si id null alors page sera egal 1 
-		    $CurrentPage = 1;
-
+			return $Count;
 		}
+	}
 
-		return $CurrentPage;
 
+	public function Prev($CurrentPage, $url) {
+		if ($CurrentPage > 1) {
+			$link = $url;
+			if ($CurrentPage > 2) {
+				$link .= "?page=" . ($CurrentPage - 1);
+			}
+			return "<li class='page-item'><a class='page-link' href='$link'><i class='fas fa-angle-double-left'></i></a></li>";
+		} else {
+			return '<li class="disabled page-item"><a class="page-link"><i class="fas fa-angle-double-left"></i></a></li>';
+		}
+	}
+
+	public function Next($CurrentPage, $pages, $url) {
+
+		if ($CurrentPage < $pages) {
+
+			return "<li class='page-item'><a class='page-link' href='$url'><i class='fas fa-angle-double-right'></i></a></li>";
+
+		} else {
+			return '<li class="disabled page-item"><a class="page-link"><i class="fas fa-angle-double-right"></i></a></li>';
+		}
 	}
 
 
 	/*
 	* return pagination view
 	*/
-	function Pager($nb=2,$page=null,$statement,$attr=null){
-	
-	
-		if(!empty($this->CountPage($statement,$attr=null) > $this->PagesMax())){
-	
-		echo '<div class="pag"><ul class="pagination mb-3 mt-3 pagination-sm">';
-		//prev
-		if($this->CurrentPage() > "1"){
-	
-			$prev = $this->CurrentPage()-1;
-			echo '<li class="page-item"><a class="page-link" href=' . $this->GetApp()->webroot() .$page.$prev.'><i class="fas fa-angle-double-left"></i></a></a></li>' ;
-	
-		}else{
-	
-			 echo '<li class="disabled page-item"><a class="page-link"><i class="fas fa-angle-double-left"></i></a></li>';
-	
-		}
-	
-		//pagination current
-	
-			for($i=1; $i <= $this->PageTotales(); $i++) {
-	
-				if($i <= $nb || $i > $this->PageTotales() - $nb ||  ($i > $this->CurrentPage()-$nb && $i < $this->CurrentPage()+$nb)){
-	
-					if($i == $this->CurrentPage()) {
-	
-						echo '<li class="page-item active"><a class="page-link">'. $i .'</a></li>';
-	
-	
-					} else {
-	
-						echo '<li class="page-item"><a class="page-link" href=' . $this->GetApp()->webroot() .$page.$i.'>'. $i .'</a></li>' ;
-	
-					}
-	
-				}else{
-					if($i > $nb && $i < $this->CurrentPage()-$nb){
-						$i = $this->CurrentPage() - $nb;
-					}elseif($i >= $this->CurrentPage() + $nb && $i < $this->PageTotales()-$nb){
-						$i = $this->PageTotales() - $nb;
-					}
-					echo '<li class="page-item"><a class="page-link" href=' . $this->GetApp()->webroot() .$page . ($i-1) .'>...</a></li>';
-				}
-	
+	public function pageFor($CurrentPage, $pages, $url){
+
+		$nb=2;
+		for($i=1; $i <= $pages; $i++){
+		  if($i <= $nb || $i > $pages - $nb ||  ($i > $CurrentPage-$nb && $i < $CurrentPage+$nb)){
+			if($i == $CurrentPage) {
+			  echo '<li class="page-item active"><a class="page-link">'. $i .'</a></li>';
+			} elseif($i == 1) {
+			  echo '<li class="page-item"><a class="page-link" href='.$url.'>'. $i .'</a></li>' ;
+			}else{
+			  echo '<li class="page-item"><a class="page-link" href='.$url.'?page='.$i.'>'. $i .'</a></li>' ;
 			}
-	
-		//next last page
-		if($this->CurrentPage() != $this->PageTotales()){
-	
-			$next = $this->CurrentPage()+1;
-	
-			echo '<li class="page-item"><a class="page-link" href=' . $this->GetApp()->webroot() .$page .$next.'><i class="fas fa-angle-double-right"></i></a></li>' ;
-	
-		}else{
-	
-			echo '<li class="page-item disabled"><a class="page-link"><i class="fas fa-angle-double-right"></i></a></li>';
-	
+		  }else{
+			if($i > $nb && $i < $CurrentPage-$nb){
+			  $i = $CurrentPage - $nb;
+			}elseif($i >= $CurrentPage + $nb && $i < $pages-$nb){
+			  $i = $pages - $nb;
+			}
+			$it = ($i-1);
+			echo "<li class='page-item'><a class='page-link' href='$url'?page='$it'>...</a></li>";
+		  }
 		}
 	
-		echo '</ul></div>';
-		}
 	}
 
 
