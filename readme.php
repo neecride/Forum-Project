6f865d36f4dc1b4df7100dbf4ecf3ed02934985b
neecride <neecride@gmail.com>
2023-02-18 22:55:59 +0100

    Create readme.php

---
+++ readme.php
@@ -0,0 +1,32 @@
+
+Pense bête 
+Utilisation de la pagination 
+
+$pagination = new App\Pagination;
+
+on compte le total et si on a des arguments la fonction fera un prepare sinon un query 
+$Count = $pagination->CountIdForpagination('SELECT COUNT(id)','argument get id par exemple');
+
+check si la page exist et que c'est bien un entier et si c'est négative 
+sinon génére une redirection avec le lien dans le parametre
+$pagination->isExistPage($router->generate($match['name'], ['id' => $params['id']]));
+
+<nav>
+<ul class="pagination mb-3 mt-3 pagination-sm">
+<?= $pagination->Prev($router->generate('forum')) ?>
+<?= $pagination->pageFor($router->generate('forum')) ?>
+<?= $pagination->Next($router->generate('forum')) ?>
+</ul>
+</nav>
+
+génére un lien qui renvois vers la page++ et la balise réponse #rep
+<a href="<?= $pagination->userLinkPage($router->generate('viewtopic', ['id' => $posts->topicid]), $rep->idrep,$count->countid) ?>">
+<?= $rep->username ?>
+</a>
+
+affiche une mini pagination avec une boucle for a coté du titre par exemple 
+le lien est généré avec le nombre de topic en bdd et le lien vers la page 
+si $i == 1 on afficche le lien sans page sinon on affiche la page 1/2/3 etc..
+<div class="uri">
+<?= $pagination->subLinkPage($router->generate('viewtopic',['id' => $posts->topicid]),$count->countid) ?>
+</div>
