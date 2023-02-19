11a8bde9febc89065aee680e1fb2c5ac095fff08
neecride <neecride@gmail.com>
2023-02-19 01:28:56 +0100

    Update Pagination.php

--- App/Pagination.php
+++ App/Pagination.php
@@ -3,7 +3,6 @@
 namespace App;
 
 use PDO;
-use App\URL;
 
 Class Pagination extends Database{
 	
@@ -37,12 +36,57 @@ Class Pagination extends Database{
 		return (int) $this->Params()->GetParam(2);
 	}
 
+
+    public function getInt(string $name, ?int $default = null): ?int
+    {
+        global $router, $match;
+        if(!isset($_GET[$name])) return $default;
+        if($_GET[$name] === '0') return 0;
+
+        if(!filter_var($_GET[$name], FILTER_VALIDATE_INT)) {
+            if(isset($match['params']) && $match['params'] != null){
+                if(isset($match['params']['slug']) && $match['params']['slug'] != null){
+                    header('Location:' . $router->generate($match['name'], ['slug' => $match['params']['slug'], 'id' => $match['params']['id']]));
+                }else{
+                    header('Location:' . $router->generate($match['name'], ['id' => $match['params']['id']]));
+                }
+            }else{
+                header('Location:' . $router->generate($match['name']));
+            }
+            setFlash("Le paramètre $name dans l'url n'est pas un entier",'orange');
+            http_response_code(301);
+            exit();
+        }
+        return (int)$_GET[$name];
+    }
+
+    public function getPositiveInt(string $name, ?int $default = null): ?int
+    {
+        global $router,$match;
+        $param = self::getInt($name, $default);
+        if($param !== null && $param <= 0){
+            if(isset($match['params']) && $match['params'] != null){
+                if(isset($match['params']['slug']) && $match['params']['slug'] != null){
+                    header('Location:' . $router->generate($match['name'], ['slug' => $match['params']['slug'], 'id' => $match['params']['id']]));
+                }else{
+                    header('Location:' . $router->generate($match['name'], ['id' => $match['params']['id']]));
+                }
+            }else{
+                header('Location:' . $router->generate($match['name']));
+            }
+            setFlash("Le paramètre $name dans l'url n'est pas un entier positif",'orange');
+            http_response_code(301);
+            exit();
+        }
+        return $param;
+    }
+
 	/*
 	* initialise la page 
 	*/
 	public function CurrentPage(): int
 	{
-		return URL::getPositiveInt('page', 1);
+		return $this->getPositiveInt('page', 1);
 	}
 
 	/*
@@ -97,18 +141,18 @@ Class Pagination extends Database{
 	public function subLinkPage(string $url,int $countid)
 	{ 
 		
-		$tt = ceil($countid/$this->Params()->GetParam(2));
+		$t = ceil($countid/$this->Params()->GetParam(2));
 
-		if($tt > 1){
-		  for($ii=1; $ii <= $tt; $ii++){ 
+		if($t > 1){
+		  for($i=1; $i <= $t; $i++){ 
 
-				if($ii == 1){
+				if($i == 1){
 
-					echo "<a href=".$url.">$ii</a>";
+					echo "<a href=".$url.">$i</a>";
 					
 				}else{
 
-					echo "<a href=".$url.'?page='. $ii.">$ii</a>";
+					echo "<a href=".$url.'?page='. $i.">$i</a>";
 				}
  
 			}
@@ -117,7 +161,7 @@ Class Pagination extends Database{
 	}
 
 	/*
-	* retourne une mini pagination 
+	* retourn une mini pagination 
 	*/
 	public function userLinkPage(string $url,int $id,int $countid)
 	{
