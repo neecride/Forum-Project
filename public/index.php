72d669abd2fe4c1965e774b384915f71d4a48f2d
neecride <neecride@gmail.com>
2023-02-23 13:12:31 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,23 +2,20 @@
 if(session_status() === PHP_SESSION_NONE){ //on verifie si les session ne sont pas déjà démmarer
     session_start(); //et on demarre les sessions
 }
-//session_regenerate_id(); //Remplace l'identifiant de session courant par un nouveau
+session_regenerate_id(); //Remplace l'identifiant de session courant par un nouveau
 //phpinfo();
 //on inclu les diferente librairie fonction etc...
 define('DS', DIRECTORY_SEPARATOR);
 define('RACINE', dirname(__DIR__));
 require RACINE.DS.'vendor'.DS.'autoload.php';
+$GetParams = new App\Parameters;
 use Ausi\SlugGenerator\SlugGenerator;
 $generator = new SlugGenerator;
-$router = new AltoRouter;
+$router = new App\Router();
 $Parsing = new App\Parsing;
-$GetParams = new App\Parameters;
+$pagination = new App\Pagination;
+$forum = new App\Forum;
 $themeForLayout = $GetParams->themeForLayout($GetParams->GetParam(3));
-/* del after dev */
-//define('DEBUG_TIME' , microtime(true));
-/*$whoops = new \Whoops\Run;
-$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-$whoops->register();*/
 
 require_once (RACINE.DS.'lib'.DS.'libs-includes.php');
 
@@ -36,28 +33,47 @@ if(isset($_GET['page']) && $_GET['page'] === '1'){
   exit();
 }
 
-if(is_array($match)){
+if(is_array($router->matchRoute())){
+  
+    $match = $router->matchRoute();
+    $getUri = explode('/', $_SERVER['REQUEST_URI']);
 
-  $params = $match['params'];
-  $getUri = explode('/', $_SERVER['REQUEST_URI']);
+    ob_start();
+    //logique
+    if($getUri[1] == 'admin'){
+      is_admin();
+    }
+    require_once RACINE.DS.'lib'.DS.'modules'.DS.$match['target'].'.func.php';
 
-  ob_start();
-  if($getUri[1] == 'admin'){
-    is_admin();
-    require RACINE.DS.'lib'.DS.'modules'.DS.$match['target'].'.func.php';
-    require RACINE.DS.'public'.DS.'templates'.DS.'dashboard-default'.DS.$match['target'].'.php';
-  }else{
-    require RACINE.DS.'lib'.DS.'modules'.DS.$match['target'].'.func.php';
-    require RACINE.DS.'public'.DS.'parts'.DS.$match['target'].'.php';
-  }
-  $contentForLayout = ob_get_clean();
-  require_once RACINE.DS.'public'.DS.'templates'.DS.$themeForLayout.'.php';
+    //si un fichier de personalisation existe on l'inclu sinon on inclu le fichier par defaut
+    if(file_exists(RACINE.DS.'public'.DS.'templates'.DS.$themeForLayout.DS.$match['target'].'.php')){
+      
+      require_once RACINE.DS.'public'.DS.'templates'.DS.$themeForLayout.DS.$match['target'].'.php';
+
+    }else{
+      
+      require_once RACINE.DS.'public'.DS.'parts'.DS.$match['target'].'.php';
+
+    }
+
+    $contentForLayout = ob_get_clean();
+
+    //si un fichier de personalisation existe on l'inclu sinon on inclu le fichier par defaut
+    if(file_exists(RACINE.DS.'public'.DS.'templates'.DS.$themeForLayout.DS.$themeForLayout.'.php')){
+      
+      require_once RACINE.DS.'public'.DS.'templates'.DS.$themeForLayout.DS.$themeForLayout.'.php';
+
+    }else{
+
+      require_once RACINE.DS.'public'.DS.'templates'.DS.$themeForLayout.'.php';
+
+    }
   
 
 }else{
 
-    setFlash('<strong>Ho ho !</strong> cette page n\'éxiste pas redirection sur la page d\'erreur','orange');
-    http_response_code(404);
-    redirect($router->generate('error'));
+  setFlash('<strong>Ho ho !</strong> cette page n\'éxiste pas redirection sur la page d\'erreur','orange');
+  http_response_code(404);
+  redirect($router->routeGenerate('error'));
 
 }
