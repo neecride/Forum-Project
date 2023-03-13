a57b9d1560127bd8815de04dc4f177f6316be95d
neecride <neecride@gmail.com>
2023-03-13 19:02:34 +0100

    Update AccountAction.php

--- App/AccountAction.php
+++ App/AccountAction.php
@@ -4,13 +4,11 @@ namespace App;
 
 Class AccountAction{
 	
-	private $count;
+	public $errors;
 	private App $app;
 	private Database $cnx;
 	private Router $router;
 	private Session $session;
-	private $validator;
-	public $errors;
 	private Parameters $parameters;
 
 	public function __construct()
@@ -29,53 +27,84 @@ Class AccountAction{
 		}
 	}
 
-	public function test(): self
+	public function editMdp(): self
 	{
-		if(isset($_POST['go'])){
-			
-			$this->app->methodPostValid('POST');
-			
-			$this->session->checkCsrf();
+		if(isset($_POST['pwd'])){
 
-			$premier = $_POST['premier'];
-			$deux = $_POST['deux'];
-			$trois = $_POST['trois'];
+			$this->app->methodPostValid('POST');
+			$this->session->checkCsrf();
 		
-			$this->validator = $this->getValidator(
-				[
-					'premier' 	=> $premier,
-					'deux' 		=> $deux,
-					'trois' 	=> $trois
-				]
-				)
-				->required('premier','deux','trois')
-				->isDifferent('deux',$trois)
-				->validName('premier')
-				->validName('trois');
+			$pass = trim($_POST['password']);
+			$password_confirm = trim($_POST['password_confirm']);
+		
 			
-			if($this->validator->isValid())
-			{
-				$this->app->setFlash("Le formulaire a bien été envoyer");
+			$validator = $this->getValidator()
+			->notEmpty($pass,$password_confirm)
+			->isDifferent($pass,$password_confirm)
+			->validMdp($pass);
+
+			if($validator->isValid()){
+				$user_id = (int) $_SESSION['auth']->id;
+				$password = trim(password_hash($pass, PASSWORD_BCRYPT));
+				$this->cnx->Request("UPDATE users SET password = ? WHERE id = ?",[$password, $user_id]);
+				$_SESSION['auth']->password = $password;
+				$this->app->setFlash('Votre mots de pass a bien étais modifier');
 				$this->app->redirect($this->router->routeGenerate('account-edit'));
 			}
-			$this->errors = $this->validator->getErrors();
-			var_dump($this->validator);
+			$this->errors = $validator->getErrors();
+		
 		}
 		return $this;
 	}
 
-	public function delAvatar(string $field): self
+	public function editEmail(): self
 	{
-		if(isset($_POST[$field])){
+		if(isset($_POST['edit-email'])){
+
+			$this->app->methodPostValid('POST');
+
+			$this->session->checkCsrf();
+		
+			$profil_id = (int) $_SESSION['auth']->id;
+		
+			$email = trim(filter_var($_POST['email'], FILTER_SANITIZE_EMAIL));
+		
+			$email_confirm = trim(filter_var($_POST['emailConfirm'], FILTER_SANITIZE_EMAIL));
+	
+			$exist = $this->cnx->request('SELECT email FROM users WHERE email = ?',[$email],1);
+
+			$validator = $this->getValidator()
+				->notEmpty($email,$email_confirm)
+				->isDifferent($email,$email_confirm)
+				->validEmail($email_confirm)
+				->validEmail($email)
+				->isReqExist($exist);
+
+			if($validator->isValid()){
+				$this->cnx->Update("UPDATE users SET email = ? WHERE id = ?",[$email, $profil_id]);
+				$this->app->setFlash('Votre email a bien étais modifier');
+				$this->app->redirect($this->router->routeGenerate('account-edit'));
+			}
+			$this->errors = $validator->getErrors();
+		
+		}
+		return $this;
+	}
+
+	public function delAvatar(): self
+	{
+		if(isset($_POST['delete-avatar'])){
+
+			$this->app->methodPostValid('POST');
 
 			$this->session->checkCsrf();
 			
 			$file = $_SESSION['auth']->avatar;
 		
-			$this->validator = $this->getValidator([$field => $file])
-				->fileExist($file);
+			$validator = $this->getValidator()
+				->fileExist($file,'inc/img/avatars/');
 			
-			if($this->validator->isValid())
+			if($validator->isValid())
 			{
 				$profil_id = intval($_SESSION['auth']->id);	
 				unlink('inc/img/avatars/' . $file);
@@ -83,14 +112,14 @@ Class AccountAction{
 				$this->app->setFlash('L\'avatar a bien été supprimer');
 				$this->app->redirect($this->router->routeGenerate('account-edit'));
 			}
-			$this->errors = $this->validator->getErrors()[$file];
+			$this->errors = $validator->getErrors();
 		}
 		return $this;
 	}
 
-	private function getValidator($request)
+	private function getValidator()
 	{
-		return new Validator($request);
+		return new Validator();
 	}
 
 	//check les utilisateur
