fe9ca40b11a87f5323d8b6cc1eeb1934d23bfc6c
neecride <neecride@gmail.com>
2023-03-13 19:02:53 +0100

    Update Validator.php

--- App/Validator.php
+++ App/Validator.php
@@ -13,22 +13,17 @@ class Validator{
 	 */
 	private $errors = [];
 	
-	/**
-	 * params
-	 *
-	 * @var array
-	 */
-	private $params;
 	private App $app;
 	private Router $router;
 	private Parameters $parameters;
+	private Database $cnx;
 
-	public function __construct(array $params)
+	public function __construct()
 	{
 		$this->app = new App;
-		$this->params = $params;
 		$this->router = new Router;
 		$this->parameters = new Parameters;
+		$this->cnx = new Database;
 	}
 	
 	/**
@@ -37,43 +32,50 @@ class Validator{
 	 * @param  mixed $keys
 	 * @return self
 	 */
-	public function required(string ...$keys): self
+	public function required(string $key): self
 	{	
-		foreach($keys as $key){
-			$value 	=  (string) $this->getValue($key);
-			if(is_null($value) || empty($value)){
-				$this->addError($key, 'required');
-			}
+		if(is_null($key)){
+			var_dump($key);
+			$this->errors = ["Tout les champs sont requis"];
 		}
-	
 		return $this;
 	}
 
-	/*
-	* si on a une traduction 
-	* return error $this->addError($key, 'slug');
-	*/
+
 	/**
 	 * isDiffent on vérifie si les valeurs sont différente
 	 *
 	 * @param  mixed $key
-	 * @param  mixed $value
+	 * @param  mixed $field
 	 * @return self
 	 */
-	public function isDifferent(string $key, string $field): self 
+	public function isDifferent(string $key, $field): self 
 	{	
-		$value 	=  (string) $this->getValue($key);
-		if($value !== $field){
-			$this->addError($key, 'isDifferent');
+		if($key != $field){
+			$this->errors = ["Les champ sont différent"];
 		}
 		return $this;
 	} 
 
-	public function fileExist(string $key): self
+	public function fileExist(string $key, string $path): self
 	{
-		$value 	=  (string) $this->getValue($key);
-		if(!file_exists('inc/img/avatars/' . $value)){
-			$this->addError($key, 'fileExist');
+		if(!file_exists($path . $key)){
+			$this->errors = ["Le fichier n'existe pas"];
+		}
+		return $this;
+	}
+
+
+	/**
+	 * isReqExist vérifie si une valeur existe déjà en base de donnée
+	 *
+	 * @param  mixed $key
+	 * @return self
+	 */
+	public function isReqExist($key): self
+	{
+		if($key){
+			$this->errors = ["Le champ existe déjà en base de donnée"];
 		}
 		return $this;
 	}
@@ -87,77 +89,36 @@ class Validator{
 	 */
 	public function validName(string $key): self
 	{
-		$value 	=  (string) $this->getValue($key);
-		var_dump($value);
-		if(!preg_match('/^[a-zA-Z0-9ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÑÒÓÔÕÖØÙÚÛÜÝàáâãäåæçèéêëìíîïñòóôõöøùúûüý_-]{3,20}$/', $value)){
-			$this->addError($key, 'validName');
+		if(!preg_match('/^[a-zA-Z0-9ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÑÒÓÔÕÖØÙÚÛÜÝàáâãäåæçèéêëìíîïñòóôõöøùúûüý_-]{3,20}$/', $key)){
+			$this->errors = ["Le champ doit contenir 3|20 caractères alphanuméric (accent compris) tirets (-) et underscores (_) pas d'espaces."];
 		}
         return $this;
 	}
 
 	public function validTtitle(string $key): self
 	{
-		$value 	=  (string) $this->getValue($key);
-		if(!preg_match('/^[a-zA-Z0-9ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÑÒÓÔÕÖØÙÚÛÜÝàáâãäåæçèéêëìíîïñòóôõöøùúûüý\-\'\!?\s]{5,50}$/',$value)){
-			$this->addError($key, 'validTitle');
+		if(!preg_match('/^[a-zA-Z0-9ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÑÒÓÔÕÖØÙÚÛÜÝàáâãäåæçèéêëìíîïñòóôõöøùúûüý\-\'\!?\s]{5,50}$/',$key)){
+			$this->errors = ["Le titre dois contenir 5|50 caractère (accent compris et espaces) et des (!|?)"];
 		}
 		return $this;
 	}
-
+	
 	public function validEmail(string $key): self
 	{
-		$value 	=  (string) $this->getValue($key);
-		if(!filter_var($value, FILTER_VALIDATE_EMAIL)){
-			$this->addError($key, 'validEmail');
+		if(filter_var($key, FILTER_VALIDATE_EMAIL) === false){
+			$this->errors = ["Les email ne sont pas valide"];
 		}
 		return $this;
 	}
 
 	public function validMdp(string $key): self
 	{
-		$value 	=  (string) $this->getValue($key);
-		if(!preg_match('/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,50}$/', $value)) {
-			$this->addError($key, 'validMdp');
+		if(!preg_match('/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,50}$/', $key)) {
+			$this->errors = ["Le mots de pass doit être composé de 8|50 caractères, de minuscules, une majuscule de chiffres et d’au moins un caractère spécial"];
 		}
 		return $this;
 	}
 
-
-	public function length(string $key, ?int $min, ?int $max = null): self
-	{
-		$value 	=  (string) $this->getValue($key);
-		$length = grapheme_strlen($value);
-			if
-			(
-				!is_null($min) &&
-				!is_null($max) &&
-				($length < $min || $length > $max)
-			)
-			{
-				$this->addError($key, 'betweenLength', [$min, $max]);
-				return $this;
-			}
-			if
-			(
-				!is_null($min) &&
-				($length < $min)
-			)
-			{
-				$this->addError($key, 'minLength', [$min]);
-				return $this;
-			}
-			if
-			(
-				!is_null($max) &&
-				($length > $max)
-			)
-			{
-				$this->addError($key, 'maxLength', [$max]);
-			}
-		return $this;
-	}
-
-		
 	/**
 	 * notEmpty vérifie que le champ n'est pas vide
 	 *
@@ -167,73 +128,22 @@ class Validator{
 	public function notEmpty(string ...$keys): self
 	{
 		foreach($keys as $key){
-			$value 	=  (string) $this->getValue($key);
-			if(is_null($value) || empty($value)){
-				$this->addError($key, 'notEmpty');
+			if(is_null($key) || empty($key)){
+				$this->errors = ["Les champ ne doivent pas être vide"];
 			}
 		}
 		return $this;
+	}
+
+	public function getErrors(): array
+	{
+		return $this->errors;
 	}
 
 	public function isValid(): bool
 	{
 		return empty($this->errors);
 	}
-	
-	/**
-	 * dateTime
-	 *
-	 * @param  mixed $key
-	 * @param  mixed $format
-	 * @return self
-	 */
-	public function dateTime(string $key, string $format = "Y-m-d H:i:s"): self
-	{
-		$value = (string) $this->getValue($key);
-		$date = DateTime::createFromFormat($format, $value);
-		$errors = DateTime::getLastErrors();
-		if($errors['error_count'] > 0 || $errors['warning_count'] > 0 || $date === null){
-			$this->addError($key, 'dateTime');
-		}
-		return $this;
-	}
-		
-	/**
-	 * getErrors recupère les erreurs
-	 *
-	 * @return ValidationError[]
-	 */
-	public function getErrors(): array
-	{
-		return $this->errors;
-	}
 
-		
-	/**
-	 * getValue
-	 *
-	 * @param  mixed $key
-	 * @return void
-	 */
-	private function getValue(string $key)
-	{
-		if(array_key_exists($key, $this->params)){
-			return $this->params[$key];
-		}
-		return null;
-	}
-	
-	/**
-	 * addError ajoute une erreur
-	 *
-	 * @param  mixed $key
-	 * @param  mixed $rule
-	 * @param  mixed $attributes
-	 * @return void
-	 */
-	private function addError(string $key, string $rule, $attributes = []): void
-	{
-		$this->errors[$key] = new ValidationError($key, $rule);
-	}
 
 } 
