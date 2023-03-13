80fb148a09ac57aad37df1719570161f8e7dfd9e
neecride <neecride@gmail.com>
2023-03-13 21:42:21 +0100

    Update viewtopic.func.php

--- lib/modules/viewtopic.func.php
+++ lib/modules/viewtopic.func.php
@@ -2,17 +2,14 @@
 
 use App\TopicAction;
 
-$topic = $forum->firstTopic($match['params']['id']); 
-
 $pagination->CountIdForpagination('SELECT COUNT(id) FROM f_topics_reponse WHERE f_topic_id = ?', $match['params']['id']);
 
 $pagination->isExistPage();
 
-$forum->getTopicExist();
-
-$Response = (new TopicAction())
+$Response = (new TopicAction($match['params']['id']))
                 ->Responses($pagination->isPage())
-                ->viewNotView($topic->f_topic_message_date,$topic->f_topic_date)
+                ->viewNotView()
                 ->resolved()
                 ->sticky()
-                ->nbView();
+                ->nbView()
+                ->getTopicExist();
