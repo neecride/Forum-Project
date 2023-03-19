8f07dd45c4e6fcd9961f6815436dfb320a76761a
neecride <neecride@gmail.com>
2023-03-20 00:10:32 +0100

    Delete public/templates/neec directory

--- public/templates/neec/parts/top/widgetAlert.php
+++
@@ -1,4 +0,0 @@
-<div class="alert-box alert-<?= $GetParams->GetParam(4, 'param_color') ?>">
-    <div class="alert-box-title"><?= $Parsing->Renderline($GetParams->GetParam(4, 'param_name')) ?></div>
-    <div class="alert-box-content"><?= $Parsing->RenderText($GetParams->GetParam(4, 'param_value')) ?></div>
-</div>
